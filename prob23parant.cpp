5f49eb96d13e8f9d43efd8c73be3f6274a695bb1
atultirkey <atultirkey54966@gmail.com>
2024-03-14 23:53:17 +0530

    prb23

---
+++ prob23parant.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+
+private:
+    void backtrack(vector<string>& result, string current, int left, int right, int n) {
+        if (current.size() == 2 * n) {
+            result.push_back(current);
+            return;
+        }
+        if (left < n) {
+            backtrack(result, current + '(', left + 1, right, n);
+        }
+        if (right < left) {
+            backtrack(result, current + ')', left, right + 1, n);
+        }
+    }
+};
